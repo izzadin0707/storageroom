a3e76b96539ca0dbad2d71912bc6e559216b600c
Dinzadin <153895119+Dinzadin@users.noreply.github.com>
2025-02-08 22:04:46 +0700

    update Home controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,8 +2,19 @@
 
 namespace App\Controllers;
 
+/**
+ * @property session session
+ */
+
 class Home extends BaseController
 {
+    public function __construct()
+    {
+        $this->session = session();
+
+        $this->session->set('menu', '');
+    }
+
     public function index(): string
     {
         if (!isset(session()->user))
