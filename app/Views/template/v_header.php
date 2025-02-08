20cc03340350603ffc70d581b5c6bed7a2124a4f
Dinzadin <153895119+Dinzadin@users.noreply.github.com>
2025-02-08 22:05:12 +0700

    update view header;

--- app/Views/template/v_header.php
+++ app/Views/template/v_header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?= !empty($_SESSION['menu']) && $_SESSION['menu'] ? ucfirst($_SESSION['menu']) : 'StorageRoom' ?></title>
     <?= $this->include('template/v_import') ?>
 </head>
 <style>
@@ -29,7 +29,7 @@
             <!-- NAVBAR -->
             <div class="bg-white shadow-sm z-2 py-2 px-3 d-flex justify-content-between align-items-center">
                 <div>
-                    <span class="fs-5 fw-semibold">Dashboard</span>
+                    <span class="fs-5 fw-semibold"><?= !empty($menu_title) && $menu_title ? ucfirst($menu_title) : '' ?></span>
                 </div>
                 <div>
                     <span id="logout-btn" class="fs-4" style="cursor: pointer;"><i class="bx bx-power-off"></i></span>
