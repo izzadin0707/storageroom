f262799e821f2d4606fde07a24136df706b9280d
Dinzadin <153895119+Dinzadin@users.noreply.github.com>
2025-02-10 00:35:14 +0700

    update route;

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,5 +12,16 @@ $routes->post('/logout-process', 'LoginController::logout', ['filter' => 'auth']
 $routes->get('/', 'Home::index');
 
 $routes->group('users', ['filter' => 'auth'], function ($routes) {
-    $routes->get('/', 'UserController::index');
+    $routes->get('/', 'Settings\UserController::index');
+    $routes->post('table', 'Settings\UserController::datatable');
+    $routes->post('save', 'Settings\UserController::save');
+    $routes->post('delete', 'Settings\UserController::delete');
+});
+
+$routes->group('role', ['filter' => 'auth'], function ($routes) {
+    $routes->get('/', 'Settings\RoleController::index');
+    $routes->post('save', 'Settings\RoleController::save');
+    $routes->post('delete', 'Settings\RoleController::delete');
+    $routes->post('table', 'Settings\RoleController::datatable');
+    $routes->post('select', 'Settings\RoleController::select');
 });
